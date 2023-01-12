ad8609c19165fef129200f3579e727e9ef14b350
Gregory Leventoux <gregory.leventoux@hotmail.fr>
2023-01-12 16:59:53 +0100

    add temp files for soutenance

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,7 @@
 import os
 import h5py
 from sklearn.decomposition import IncrementalPCA
+import dask.array as da
 
 
 # TEST FUNCTIONS DEFINITION
@@ -16,7 +17,6 @@ def test_sklearn_ipca(filename):
     '''
 
     file_path = os.path.join("matrix",filename)
-    print(file_path)
 
     with h5py.File(file_path,"r") as f:
 
@@ -24,6 +24,32 @@ def test_sklearn_ipca(filename):
 
         for batch in f.keys():
             ipca.partial_fit(f[batch])
+
+@profile
+def test_dask_svd(filename):
+
+    '''
+    Parameters
+    ----------
+    filename : str
+               Name of the .hdf5 file storing the matrix with the extension
+
+    '''
+
+    file_path = os.path.join("matrix",filename)
+    
+    with h5py.File(file_path,"r") as f:
+
+        data = []
+
+        for batch in f.keys():
+
+            batch_shape = f[batch].shape
+            data.append(da.from_array(f[batch],chunks=batch_shape))
+
+    x = da.concatenate(data,axis=0)
+
+    u, s, v = da.linalg.svd(x)
 
 
 # TEST FUNCTIONS CALL
@@ -34,5 +60,6 @@ filename_list.remove(".gitkeep")
 for filename in filename_list:
 
     test_sklearn_ipca(filename)
+    test_dask_svd(filename)
 
 
