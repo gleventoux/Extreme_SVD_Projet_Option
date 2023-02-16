ea53b8948c5e4c465aadd90a81e007c7bffacf34
XinZhang <zhangxin@ZX-MacbookAir.local>
2023-02-16 13:48:47 +0100

    add svd_functions

--- svd_func.py
+++ svd_func.py
@@ -4,6 +4,15 @@ import sys
 import numpy as np
 from mpi4py import MPI
 import h5py
+import subprocess
+
+num_processes = 6 
+script_file = 'test.py'  
+cmd_list = ['mpirun', '-np', str(num_processes), 'python3', script_file, 'arg1', 'arg2', ...]
+result = subprocess.run(cmd_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+print(result.stdout.decode('utf-8'))
+print(result.stderr.decode('utf-8'))
 
 # Define the __file__ global variable
 __file__ = "<path_to_file>"
@@ -42,7 +51,6 @@ def svd_func_template(matrix_filename, decomposition_dir,vectors = True):
    """
 
 
-
 # method for importing data from .h5 files into the forms suitable for pyparsvd serial calculation
 def load_split(filename,datasetname,split=5):
     """
@@ -53,7 +61,7 @@ def load_split(filename,datasetname,split=5):
     Args:
         filename   : the path to the HDF5 file that contains the dataset
         datasetname : the name of the dataset in HDF5 file
-        
+
     Returns:
         a portion of the dataset
     """    
@@ -119,20 +127,63 @@ def load_h5_parallel(filename,comm,rank,nprocs,dataset):
     h5_file.close()
     return rval
 
-def pypar_serial(matrix_filename, decomposition_dir,split=5,random=False,vectors = True):
-    
+def load_h5_serial(filename,datasetname):
     pass
 
+def load_h5_parallel(filename,comm,rank,nprocs,dataset):
+    """
+    This code loads a dataset from an HDF5 file and 
+    returns a portion of the dataset based on the rank 
+    and number of processes of the calling MPI process.
 
+    Args:
+        filename   : the path to the HDF5 file that contains the dataset
+        comm   : the MPI communicator object that is used to communicate between the MPI processes.
+        rank   : the rank of the calling MPI process
+        nprocs   : the number of MPI processes that are running the code.
+        dataset   : the name of the dataset in the HDF5 file that should be loaded
 
-
-def pypar_parallel(matrix_filename, decomposition_dir,random=False,vectors = True):
+    Returns:
+        a portion of the dataset
+    """
+    h5_file = h5py.File(filename, 'r', driver='mpio', comm=comm)
+    dset = h5_file[dataset]
+    ndof=dset.shape[0]
+    num_rows_rank = int(ndof/nprocs)
     
-    pass
+    if rank != nprocs-1:
+        rval =  dset[rank*num_rows_rank:(rank+1)*num_rows_rank,:].reshape(num_rows_rank,-1)
+    else:
+        num_dof_local = ndof - rank*num_rows_rank
+        rval =  dset[rank*num_rows_rank:,:].reshape(num_dof_local,-1)
+    h5_file.close()
+    return rval
 
+def pypar_serial(data_list, results_dir='./decomposition_results',random=False,vectors = True,K=10,ff=1.0):
+    SerSVD = ParSVD_Serial(K=K, ff=ff,low_rank=random,results_dir=results_dir)    
+    data_init=data_list.pop(0)
+    data_init=load_h5_serial(data_init,'dataset')
+    SerSVD.initialize(data_init)
+    for data in data_list:
+        data=load_h5_serial(data,'dataset')
+        SerSVD.incorporate_data(data) 
+    SerSVD.save()
 
+def pypar_parallel(data_list, results_dir='./decomposition_results',random=False,vectors = True,K=10,ff=1.0):
+    ParSVD = ParSVD_Parallel(K=K, ff=ff, low_rank=random,results_dir=results_dir)
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    nprocs = comm.Get_size()
+    data_init=data_list.pop(0)
+    data_init=load_h5_parallel(data_init,'dataset')
+    ParSVD.initialize(data_init)
+    for data in data_list:
+        data=load_h5_parallel(data,'dataset')
+        ParSVD.incorporate_data(data)
+    ParSVD.save()
     
 if __name__ == '__main__':
-    pypar_serial()
-    pypar_parallel()
+    data_list=[]
+    pypar_serial(data_list)
+    pypar_parallel(data_list)
     
