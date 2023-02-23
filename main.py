3a81ce15e6aef98acd3fb9847f038aaf4d1cf812
gleventoux <gregory.leventoux@hotmail.fr>
2023-02-23 23:01:00 +0100

    os.path used to compute matrix paths

--- main.py
+++ main.py
@@ -2,21 +2,20 @@
 import funcutils
 import svd_func
 import gc
+import os
 
 
 
 # Matrix list definition
 matrix_list = [] # list of string, each being the name of the file associated with an matrix, extension included
 
-path_to_matrix_dir = '/matrix/' # Assume linux system path
-
-matrix_list_with_dir = [ path_to_matrix_dir + matrix for matrix in matrix_list]
-
 # Initialisation of global varaible and constants
 benchmark_results = {}
 NUMBER_OF_RUNS = 3
 
 for matrix in matrix_list_with_dir :
+    
+    matrix = os.path.join("matrix",matrix) # path to matrix file
     
     # TODO for each svd_decomposition function, 
     # write the following line adapted to each decomposition function
